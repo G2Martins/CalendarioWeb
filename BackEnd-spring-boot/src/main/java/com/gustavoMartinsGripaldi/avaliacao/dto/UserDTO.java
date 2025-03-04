4bf506a2c5e2b4237a51a92ab29a9a53571dc0b8
G2Martins <gustavogripaldi@outlook.com>
2025-03-04 15:27:56 -0300

    add: Comentario DTO

--- BackEnd-spring-boot/src/main/java/com/gustavoMartinsGripaldi/avaliacao/dto/UserDTO.java
+++ BackEnd-spring-boot/src/main/java/com/gustavoMartinsGripaldi/avaliacao/dto/UserDTO.java
@@ -1,7 +1,7 @@
 package com.gustavoMartinsGripaldi.avaliacao.dto;
 
 import com.gustavoMartinsGripaldi.avaliacao.model.User;
-
+//DTO (Data Transfer Object) é um padrão de projeto usado para transferir dados entre camadas de um sistema
 public class UserDTO {
     
     private String nome;
@@ -15,8 +15,6 @@ public class UserDTO {
         this.email = email;
         this.senha = senha;
     }
-
-   
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
